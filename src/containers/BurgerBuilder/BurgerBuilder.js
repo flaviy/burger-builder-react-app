4efbc0656a48228a508ab8edf9f08b92df234d31
Oleksandr Chebernin <chebernin@gmail.com>
2018-11-01 21:38:14 +0100

    burger with static ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
 
 
 class BurgerBuilder extends Component {
+
+/*
+    constructor(props) {
+        super(props);
+        this.state = {...}
+    }
+*/
+
+    state = {
+        ingredients : {
+            salad : 2,
+            bacon : 2,
+            meat: 2,
+            cheese : 1
+        }
+    }
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger ingredients = {this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
